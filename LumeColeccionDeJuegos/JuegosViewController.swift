588f04775799d7a83709fc4b9de8789bd5bfddf8
Edwin Arturo Lume Ventura <edwin.lume@tecsup.edu.pe>
2023-05-18 14:29:59 -0500

    Se agregó la funcionalidad de poder mover los elementos de la lista y eliminarlos.

--- LumeColeccionDeJuegos/JuegosViewController.swift
+++ LumeColeccionDeJuegos/JuegosViewController.swift
@@ -11,14 +11,32 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var tituloTextField: UITextField!
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
+    @IBOutlet weak var eliminarBoton: UIButton!
     
     var imagePicker = UIImagePickerController()
+    
+    var juego:Juego? = nil
+    
+//    var categoriaSeleccionada: Categoria? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-
-        // Do any additional setup after loading the view.
+        
+        if juego != nil {
+            imageView.image = UIImage(data: (juego!.imagen!) as Data)
+            tituloTextField.text = juego!.titulo
+            agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+        } else {
+            eliminarBoton.isHidden = true
+        }
+    }
+    @IBAction func eliminarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
     }
     
     @IBAction func fotosTapped(_ sender: Any) {
@@ -30,11 +48,17 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
 
     @IBAction func agregarTapped(_ sender: Any) {
-        let context = (UIApplication.shared.delegate as!
-                       AppDelegate).persistentContainer.viewContext
-        let juego = Juego(context: context)
-        juego.titulo = tituloTextField.text
-        juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        
+        if juego != nil {
+            juego!.titulo! = tituloTextField.text!
+            juego!.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        } else {
+            let context = (UIApplication.shared.delegate as!
+                           AppDelegate).persistentContainer.viewContext
+            let juego = Juego(context: context)
+            juego.titulo = tituloTextField.text
+            juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController?.popViewController(animated: true)
     }
--- LumeColeccionDeJuegos/ViewController.swift
+++ LumeColeccionDeJuegos/ViewController.swift
@@ -34,8 +34,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let juego = juegos[sourceIndexPath.row]
+        juegos.remove(at: sourceIndexPath.row)
+        juegos.insert(juego, at: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let juego = juegos[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(juego)
+            juegos.remove(at: indexPath.row)
+            do {
+                try context.save()
+            } catch {
+                print("Error al guardar el contexto: \(error)")
+            }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.isEditing = true
         tableView.dataSource = self
         tableView.delegate = self
     }
